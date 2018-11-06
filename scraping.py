7a18e119311c880b02a80afa3a01e36817e6f947
MLaburu <mikel.lyh@gmail.com>
2018-11-06 20:40:34 +0100

    Scrapper base funcional

--- scraping.py
+++ scraping.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup
+from registro import Registro
 import requests
+import csv
+import re
 # Here, we're just importing both Beautiful Soup and the Requests library
 
 page_link = 'https://www.eltiempo.es/bilbao.html?v=por_hora'
@@ -11,6 +14,59 @@ page_response = requests.get(page_link, timeout=5)
 page_content = BeautifulSoup(page_response.content, "html.parser")
 #we use the html parser to parse the url content and store it in a variable.
 
+def preprocesarTormentas(tormenta):
+    # Eliminar saltos de linea y tabulaciones
+    tormenta = re.sub(r'[\n\t]', "", tormenta)
+    # Eliminar múltiples espacios
+    tormenta = re.sub(' +',' ',tormenta)
+    return tormenta
+
+def limpiarRegistros(registros):
+    for i in range(len(registros)):
+        # Si es el primer elemento de la lista, eliminar el antepenúltimo
+        if i == 0:
+            registros[i].pop(len(registros[i])-4)
+        # De lo contrario, borrar el último elemento 
+        else:
+            registros[i].pop(len(registros[i])-1)
+    return registros
+
+def preprocesarRegistros(registros):
+    """Funcion que se encarga de limpiar la lista que se obtiene de múltiples registros"""
+    l = []
+    l2 = []
+    # Dividir la lista en listas en función de los registros
+    for i in range(len(registros)):
+        if registros[i] != "Humedad":
+            l2.append(registros[i])
+        else:
+            if l2 != []:
+                l.append(l2)
+                l2 = []
+            l2.append('Humedad')
+        if i == len(registros)-1:
+            l.append(l2)
+    l = limpiarRegistros(l)
+    return l
+
+def saveCSV(registros, dias):
+    j=0
+    with open("/home/mikel/Master/Tipología_y_ciclo_de_vida_de_los_datos/Practica1/Test/Dataset.csv", 'w', encoding='utf8') as mycsv:
+        wr = csv.writer(mycsv)
+        wr.writerow(['Dia','Hora','Previsión','Velocidad','Rachas','Lluvias','Nieve','Nubes','Tormenta','Humedad','Presión','Sensación térmica','Prob. precipitación','Hora observación', 'Visibilidad'])
+        for i in range(len(horas)):
+            if horas[i] == '00:00':
+                j = j + 1
+            registro = registros[i]
+            if i == 0:
+                wr.writerow([dias[j], horas[i],previsiones[i], velocidades[i], rachas[i], lluvias[i], nieves[i], nubes[i], tormentas[i], registro[1], registro[3], registro[7], 'NULL', registro[5], registro[9]])
+            else:
+                if 'Prob. de precipitación' in registro:
+                    wr.writerow([dias[j], horas[i],previsiones[i], velocidades[i], rachas[i], lluvias[i], nieves[i], nubes[i], tormentas[i], registro[1], registro[3], registro[7], registro[5], 'NULL', 'NULL'])            
+                else:
+                    wr.writerow([dias[j], horas[i],previsiones[i], velocidades[i], rachas[i], lluvias[i], nieves[i], nubes[i], tormentas[i], registro[1], registro[3], registro[5], 'NULL', 'NULL', 'NULL'])            
+
+#Dias
 dias = []
 for i in range(0, 4):
     title = page_content.find_all("h2")[i].text
@@ -24,15 +80,11 @@ horas = [hora.text for hora in horas if hora.text != 'Horas']
 previsiones = page_content.findAll('div',attrs={"class":"m_table_weather_hour_detail_pred"})
 previsiones = [prevision.text for prevision in previsiones if prevision.text != 'Previsión']
 
-#Vientos
-vientos = page_content.findAll('div',attrs={"class":"m_table_weather_hour_detail_wind"})
-vientos = [viento.text for viento in vientos if viento.text != 'Viento']
-
 #Velocidades
 velocidades = page_content.findAll('div',attrs={"class":"m_table_weather_hour_detail_med"})
 velocidades = [velocidad.text for velocidad in velocidades if velocidad.text != 'Velocidad']
 
-#Vientos
+#Rachas
 rachas = [racha.text for datos in page_content.findAll('div',attrs={"class":"m_table_weather_hour_detail_gust m_table_weather_hour_detail_child_mobile"}) for racha in datos.findAll('span', attrs={"data-wind-include-units":"1"})]
 
 #Lluvias
@@ -45,28 +97,13 @@ nieves = [nieve.text for datos in page_content.findAll('div',attrs={"class":"m_t
 nubes = [nube.text for datos in page_content.findAll('div',attrs={"class":"m_table_weather_hour_detail_clouds m_table_weather_hour_detail_child_mobile"}) for nube in datos.findAll('span') if nube.text != "Nubes"]
 
 #Tormentas
-tormentas = [tormenta.text for datos in page_content.findAll('div',attrs={"class":"m_table_weather_hour_detail_thunder m_table_weather_hour_detail_child_mobile"}) for tormenta in datos.findAll('span') if tormenta.text != "Tormenta"]
+tormentas = [preprocesarTormentas(tormenta.text) for datos in page_content.findAll('div',attrs={"class":"m_table_weather_hour_detail_thunder m_table_weather_hour_detail_child_mobile"}) for tormenta in datos.findAll('span') if tormenta.text != "Tormenta"]
+print(tormentas)
 
-campos = ['Humedad', 'Presión', 'Hora observación', 'Sensación térmica', 'Visibilidad', 'Prob. de precipitación']
-registros = [datosInternos.text for datos in page_content.findAll('div', attrs={"class":"m_table_weather_hour_detail_child"}) for datosInternos in datos.findAll('span') if datosInternos.text not in campos]
 
-# first posee la humedad, presión, la hora de observación, la sensación térmica y la visibilidad del primer registro
-first = registros[:6]
+#Humedad, Presion, Sensacion terminca, Prob. precipitación, Hora observación, Visibilidad
+registros = [datosInternos.text for datos in page_content.findAll('div', attrs={"class":"m_table_weather_hour_detail_child"}) for datosInternos in datos.findAll('span')]
+registros = preprocesarRegistros(registros)
 
-# Registros posee, las humedades, presiones, probabilidades de precitacion y las sentaciones termincas
-# de todos los registros menos el primero
-registros = registros[6:]
-registros = [registros[i:i+5] for i in range(0,len(registros),5)]
 
-#Humedades
-humedades = [registro[0] for registro in registros]
-
-#Presiones
-presiones = [registro[1] for registro in registros]
-
-#Probabilidad de precipitaciones
-probPrecipitaciones = [registro[2] for registro in registros]
-
-#Sensacion térmica
-sensTermincas = [registro[3] for registro in registros]
-
+saveCSV(registros, dias)
